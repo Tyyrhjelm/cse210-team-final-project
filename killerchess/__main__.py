f7126d2f93ce3d56ae74a02bbe533142958a7163
Mario1355 <91709155+Mario1355@users.noreply.github.com>
2021-12-11 20:43:50 -0700

    Fixed formatting

--- killerchess/__main__.py
+++ killerchess/__main__.py
@@ -1,11 +1,11 @@
 from game.director import Director
 
 
-def main():
+def __main__():
     
     _director = Director()
     _director.StartGame()
 
 if __name__ == '__main__':
-    main()
+    __main__()
     
